f9e8055739743122b5d3ada9c35e75dfefb6fc60
Snehasish Roy <snehasishroy39@gmail.com>
2021-07-25 07:47:49 +0530

    Update priority queue solution for classical problem MeetingsRoom2

--- src/main/java/MeetingsRoom2.java
+++ src/main/java/MeetingsRoom2.java
@@ -1,14 +1,23 @@
-import common.Interval;
-
-import java.util.*;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.TreeMap;
 
 /**
+ * https://leetcode.com/problems/meeting-rooms-ii/ Premium
+ *
  * Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei),
  * find the minimum number of conference rooms required.
- * <p>
- * For example,
- * Given [[0, 30],[5, 10],[15, 20]],
- * return 2.
+ *
+ * Input: intervals = [[0,30],[5,10],[15,20]]
+ * Output: 2
+ *
+ * Input: intervals = [[7,10],[2,4]]
+ * Output: 1
+ *
+ * Constraints:
+ * 1 <= intervals.length <= 10^4
+ * 0 <= start_i < end_i <= 10^6
  */
 public class MeetingsRoom2 {
 
@@ -17,21 +26,22 @@ public class MeetingsRoom2 {
      * end time of previous interval e.g {2,8},{5,15},{10,20}
      * we need to remove all intervals ending before the start of current interval, we can do that by priority queue.
      * <p>
-     * {@link CarPooling} for similar problem
+     * {@link CarPooling} {@link TheNumberOfTheSmallestUnoccupiedChair}
      */
-    public int minMeetingRooms(Interval[] intervals) {
-        Arrays.sort(intervals, Comparator.comparingInt(a -> a.start));
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        minHeap.add(intervals[0].end); //we are only interested in end time
-        for (int i = 1; i < intervals.length; ++i) {
-            if (minHeap.peek() <= intervals[i].start) {
+    public int minMeetingRoomsPriorityQueue(int[][] intervals) {
+        Arrays.sort(intervals, (o1, o2) -> Integer.compare(o1[0], o2[0])); //sort the array based on start time
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[1], o2[1]));
+        //sort the pq based on end time, as we are interested in finding meetings that end before the current meeting starts
+        int maxRooms = 0;
+        for (int i = 0; i < intervals.length; ++i) {
+            while (!pq.isEmpty() && pq.peek()[1] <= intervals[i][0]) {
                 //if the current interval starts after any finished meeting, remove the finished meeting
-                //any part is important, that's why we used priority queue, not stack
-                minHeap.poll();
+                pq.poll();
             }
-            minHeap.add(intervals[i].end);
+            pq.add(intervals[i]);
+            maxRooms = Math.max(maxRooms, pq.size());
         }
-        return minHeap.size();
+        return maxRooms;
     }
 
     /**
@@ -39,7 +49,7 @@ public class MeetingsRoom2 {
      * <p>
      * {@link EmployeeFreeTime} {@link IntervalListIntersections} related problems
      */
-    public int minMeetingRooms(int[][] intervals) {
+    public int minMeetingRoomsLineSweep(int[][] intervals) {
         int max = 0, cur = 0;
         TreeMap<Integer, Integer> map = new TreeMap<>();
         for (int[] interval : intervals) {
